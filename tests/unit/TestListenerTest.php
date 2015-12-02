bc44b6d12536ef6f6abffae0c8c3e6d457e7baea
Samuel Parkinson <sam.james.parkinson@gmail.com>
2015-12-02 10:50:07 +0000

    Add file header to the test case.

--- tests/unit/TestListenerTest.php
+++ tests/unit/TestListenerTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This file is part of graze/hamcrest-test-listener.
+ *
+ * Copyright (c) 2015 Nature Delivered Ltd. <https://www.graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://github.com/graze/hamcrest-test-listener/blob/master/LICENSE MIT
+ * @link https://github.com/graze/hamcrest-test-listener
+ */
+
 use Hamcrest\Adapter\PHPUnit\TestListener;
 use Hamcrest\MatcherAssert;
 
